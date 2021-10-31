2769f8e39854601547d3a21813b268adbc2f5c4b
陈家齐 <alanjiach@gmail.com>
2021-10-30 23:57:36 -0500

    Ski Finished

--- Usaco Practice/School Season August 2021/Photoshoot.cpp
+++ Usaco Practice/School Season August 2021/Photoshoot.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -66,3 +67,4 @@ int main(){
     }
     return 0;
 }
+*/
--- XCamp201/Hw7/Ski.cpp
+++ XCamp201/Hw7/Ski.cpp
@@ -8,9 +8,9 @@ int **maze, **visited;
 int Path_Row[4] = { 0, 0, 1, -1 };
 int Path_Col[4] = { 1, -1, 0, 0 };
 
-int rows, cols, curNum, ans = 0;
+int rows, cols, ans = 0;
 
-bool CanWeMove(int rowNew, int colNew)
+bool CanWeMove(int rowNew, int colNew, int curNum)
 {
     if ((rowNew >= 0) && (rowNew < rows) && (colNew >= 0) && (colNew < cols) && (curNum > maze[rowNew][colNew]) && (visited[rowNew][colNew] == 0) )
         return true;
@@ -18,63 +18,56 @@ bool CanWeMove(int rowNew, int colNew)
     return false;
 }
 
-void FindPathInMaze(int row, int col, int desRow, int desCol, int move)
+void FindPathInMaze(int row, int col, int move)
 {
-    curNum = maze[row][col];
-
-    if ((row == desRow) && (col == desCol))
-    {
-        if(ans < move){
-            ans = move;
-        }
-        cout << move << endl;
-        cout << "//////////////////////////////////////////////////" << endl;
+    int curNum = maze[row][col];
+    if(ans < move){
+        ans = move;
     }
-    else
-        for (int index = 0; index < 4; index++)
-        {
-            int rowNew = row + Path_Row[index];
-            int colNew = col + Path_Col[index];
+    for (int index = 0; index < 4; index++)
+    {
+        int rowNew = row + Path_Row[index];
+        int colNew = col + Path_Col[index];
 
-            cout << curNum << '\n';
-            if (CanWeMove(rowNew, colNew)){
-                move++;
-                visited[rowNew][colNew] = move;
-                FindPathInMaze(rowNew, colNew, desRow, desCol, move);
-                move--;
-                visited[rowNew][colNew] = 0;
-            }
+        if (CanWeMove(rowNew, colNew, curNum)){
+            move++;
+            visited[rowNew][colNew] = move;
+            FindPathInMaze(rowNew, colNew, move);
+            move--;
+            visited[rowNew][colNew] = 0;
         }
+    }
 }
 
 int main() {
     cin >> rows >> cols;
 
-    int desX, desY;
+    maze = new int*[rows];
+    visited = new int*[rows];
 
-    maze = new int*[cols];
-    visited = new int*[cols];
-
-    for(int i = 0; i < cols; ++i){ //Input
-        maze[i] = new int[rows];
-        visited[i] = new int[rows];
-        for(int j = 0; j < rows; ++j){
+    for(int i = 0; i < rows; ++i){ //Input
+        maze[i] = new int[cols];
+        visited[i] = new int[cols];
+        for(int j = 0; j < cols; ++j){
             cin >> maze[i][j];
-            if(maze[i][j] == 1){
-                desX = j;
-                desY = i;
-            }
             visited[i][j] = 0;
         }
     }
 
-    for(int i = 0; i < cols; ++i){
-        for(int j = 0; j < rows; ++j){
-            FindPathInMaze(i, j, desX, desY, 0);
+    for(int i = 0; i < rows; ++i){
+        for(int j = 0; j < cols; ++j){
+            if(maze[i][j] > ans){
+                FindPathInMaze(i, j, 1);
+                for(int x = 0; x < rows; ++x){
+                    for(int y = 0; y < cols; ++y){
+                        visited[x][y] = 0;
+                    }
+                }
+            }
         }
     }
 
     cout << ans;
     return 0;
 }
- */
+*/
